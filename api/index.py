38b1c6c21f448518a84600d4726f241719a3717a
JJDiaz <jose@jjdiaz.dev>
2023-03-20 05:29:49 +0100

    Added Redis (Upstash) & added logo powered by vercel

--- api/index.py
+++ api/index.py
@@ -1,15 +1,27 @@
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
 import os
 import random
+import redis
 import string
-from flask import Flask, render_template, request
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
+
+# Set up Redis client
+upstash_redis_url = os.environ.get('UPSTASH_REDIS_URL')
+redis_client = redis.from_url(upstash_redis_url)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     prompt = ""
     split_length = ""
     file_data = []
+    
+    redis_client.incr("visit_counter")
+    visit_count = int(redis_client.get("visit_counter"))
 
     if request.method == "POST":
         prompt = request.form["prompt"]
@@ -19,7 +31,7 @@ def index():
         
     hash_value = generate_random_hash(8)
     
-    return render_template("index.html", prompt=prompt, split_length=split_length, file_data=file_data, hash=hash_value)
+    return render_template("index.html", prompt=prompt, split_length=split_length, file_data=file_data, hash=hash_value, visit_count=visit_count)
             
 def split_prompt(text, split_length):
     if split_length <= 0:
@@ -48,3 +60,6 @@ def split_prompt(text, split_length):
 
 def generate_random_hash(length):
     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
