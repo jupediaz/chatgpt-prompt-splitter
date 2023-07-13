a3dea2f16657b297927131219ab2d1deb8d8e8ee
EdgarAllenPoe <EdgarAllenPoe@users.noreply.github.com>
2023-07-13 12:45:28 -0700

    Update index.py
    Prevent splitting mid sentence.

--- api/index.py
+++ api/index.py
@@ -36,27 +36,33 @@ def index():
 def split_prompt(text, split_length):
     if split_length <= 0:
         raise ValueError("Max length must be greater than 0.")
-
-    num_parts = -(-len(text) // split_length)
+        
+    sentences = text.split('. ')
     file_data = []
-
-    for i in range(num_parts):
-        start = i * split_length
-        end = min((i + 1) * split_length, len(text))
-
-        if i == num_parts - 1:
-            content = f'[START PART {i + 1}/{num_parts}]\n' + text[start:end] + f'\n[END PART {i + 1}/{num_parts}]'
-            content += '\nALL PARTS SENT. Now you can continue processing the request.'
+    content = ""
+    part = 1
+    for i in range(len(sentences)):
+        # check if adding this sentence will exceed the split length
+        if len(content + sentences[i]) > split_length:
+            # finalize this part and start a new one
+            file_data.append({
+                'name': f'split_{str(part).zfill(3)}.txt',
+                'content': content
+            })
+            content = sentences[i]
+            part += 1
         else:
-            content = f'Do not answer yet. This is just another part of the text I want to send you. Just receive and acknowledge as "Part {i + 1}/{num_parts} received" and wait for the next part.\n[START PART {i + 1}/{num_parts}]\n' + text[start:end] + f'\n[END PART {i + 1}/{num_parts}]'
-            content += f'\nRemember not answering yet. Just acknowledge you received this part with the message "Part {i + 1}/{num_parts} received" and wait for the next part.'
-
-        file_data.append({
-            'name': f'split_{str(i + 1).zfill(3)}_of_{str(num_parts).zfill(3)}.txt',
-            'content': content
-        })
+            # add the sentence to the current part
+            content += sentences[i]
+    
+    # add the final part
+    file_data.append({
+        'name': f'split_{str(part).zfill(3)}.txt',
+        'content': content
+    })
 
     return file_data
+
 
 def generate_random_hash(length):
     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
